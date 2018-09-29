b9080d929023e984d194325a19dc9812b02040e7
IndranilRay <36516665+IndranilRay@users.noreply.github.com>
2018-09-30 00:41:30 +0530

    Recommending best match for any given word.

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
 import json
+from difflib import get_close_matches
 
 data = json.load(open("data.json"))
 
 
 def translate(input_word):
     # lower case all the input string provided
-
     input_word = input_word.lower()
 
     if input_word in data:
         return data[input_word]
+    elif len(get_close_matches(input_word, data.keys())) > 0:
+        return "You mean %s instead" % get_close_matches(input_word, data.keys())[0]
     else:
         return "The word does'nt exist.Please check it again!."
 
